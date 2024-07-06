dc4e09bb5d173df635aa693f120d103205c43769
VaishooR <vaishooramkrish@gmail.com>
2024-07-06 19:18:28 +0530

    minimum of 3 numbers

--- Problems/12-MinimumOf3Nums.js
+++ Problems/12-MinimumOf3Nums.js
@@ -21,3 +21,30 @@ function MinimumOfThree(num1,num2,num3){
     return min
 }
 console.log(MinimumOfThree(10,20,30))
+
+
+
+
+// APPROACH-3
+function minimumOf3Numbers1(a,b,c){
+    return (a < b) ? (a < c ? a : c) : (b < c ? b : c) 
+}
+console.log(minimumOf3Numbers1(41,22,32))
+
+
+
+
+// APPROACH-4
+function findMin(num1,num2,num3){
+    if(num1<num2 && num1<num3){
+      console.log("Minimum Num is :",num1)
+      return;
+    }else if(num2<num1 && num2<num3){
+      console.log("Minimum Num is :",num2)
+      return;
+    }else if(num3<num1 && num3<num2){
+      console.log("Minimum Num is :",num3)
+      return;
+    }
+}
+findMin(122,52,23)

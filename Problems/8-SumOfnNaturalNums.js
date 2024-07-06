1f9bc187fa6fc487e80992edd7a85b80273dbf0e
VaishooR <vaishooramkrish@gmail.com>
2024-07-06 10:30:27 +0530

    sum of n natural numbers

---
+++ Problems/8-SumOfnNaturalNums.js
@@ -0,0 +1,14 @@
+// Write a program to calculate the sum of the first 10 natural numbers.
+
+function sum(n){
+    if(n<1){
+        console.log("Invalid number")
+        return;
+    }
+    let result=0;
+    for(let i=1; i<=n; i++){
+        result += i
+    }
+    console.log(`Sum of first ${n} natural numbers is = ${result}`);
+ }
+ sum(10)
